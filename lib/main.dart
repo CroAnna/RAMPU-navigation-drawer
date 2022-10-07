72dc6fbfd70432d1e7ef4f70472a26705fe232d6
Ana Skarica <ana.skarica.as@gmail.com>
2022-10-07 12:28:41 +0200

    dodani listtileovi u listviewu navigacije i podijeljeno u 2 dijela da funkcionira zatvaranje na klik na listtile

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        drawer: const Drawer(),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.purple),
+        home: const RootPage());
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Navigation Drawer from Left',
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 223, 223, 223),
+              ),
+              child: Text("Izbornik"),
+            ),
+            ListTile(
+              leading: Icon(Icons.house),
+              title: Text("Odabir 1"),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.park),
+              title: Text("Odabir 2"),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.holiday_village),
+              title: Text("Odabir 3"),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
